6cba0f8fc9ecf9f109d7a252f3bb179be0dfe1d6
Blondnv <blondnv@gmail.com>
2025-10-27 15:36:51 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const app = express();
+
+const target = "http://213.35.123.50";
+
+app.use("/", createProxyMiddleware({
+  target,
+  changeOrigin: true,
+  onProxyReq(proxyReq, req, res) {
+    proxyReq.setHeader("Host", req.headers.host);
+    proxyReq.setHeader("X-Real-IP", req.headers["x-real-ip"] || req.socket.remoteAddress);
+  },
+}));
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Reverse proxy running on port ${port}`);
+});
